ae67383e86b516016a5b29e55608bc172b7ed9d4
melvin-rulit <marchenko1985g@outlook.com>
2023-12-22 21:11:30 +0200

    рефакторинг

--- db.py
+++ db.py
@@ -7,8 +7,22 @@ class Database:
     def connect(self):
         self.connection = mysql.connector.connect(
             host='localhost',
-            user='myuser',
-            password='mypassword',
-            database='mydatabase'
+            user='root',
+            password='',
+            database='massag_Bot',
+            port=3307
         )
         self.cursor = self.connection.cursor()
+        
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+            
+    def insert_users(self, name, birthday, email, password, photo_patch):
+        with self.connection:
+            self.cursor.execute(
+                "insert into clients (client_id, name, lang, birthday, email, password, photo_patch) values (1212, %s, 'ru', %s, %s, %s, %s)",
+                (name, birthday, email, password, photo_patch))
+            self.connection.commit()
+            self.cursor.close()
